21b9e71c35319321d42cc862d692d5c34e0bcc80
Gabriel Guimaraes <gabriel@brex.com>
2021-08-12 14:54:50 -0300

    8 digit hashes

--- blockchain.py
+++ blockchain.py
@@ -2,10 +2,17 @@ from hashlib import sha256
 import copy
 import json
 
+class Hashable:
+    @staticmethod
+    def hash_fn(string): return sha256(string).hexdigest()[:8]
+
+    def hash(self): return Hashable.hash_fn(self.__str__())
+
+
 # An account is the main piece of state in Ethereum
 # A regular externally owned account holds a balance (just like a Bitcoin account),
 # but a Contract account also holds code and a data storage
-class Account:
+class Account(Hashable):
     EXTERNALLY_OWNED = 'externally_owned'
     CONTRACT = 'contract'
 
@@ -24,14 +31,11 @@ class Account:
         ret = 'Contract ' if self.type() == self.CONTRACT else ''
         return ret + 'Account: {0}\nNonce: {1}, Balance: {2}\nCode: {3}\nStorage: {4}\n'.format(self.address, self.nonce, self.balance, self.code, self.storage)
 
-    def hash(self):
-        return sha256(self.__str__()).hexdigest()
-
     def call_contract(self, args):
         exec(self.code, {'storage': self.storage, 'args': args})
 
 # The state of the entire world is just a set of Accounts
-class WorldState:
+class WorldState(Hashable):
     def __init__(self, accounts):
         self.accounts = accounts
 
@@ -45,7 +49,7 @@ class WorldState:
         return next((a for a in self.accounts.values() if a.creation_tx_hash == creation_tx_hash), None)
 
 # A block is just a bunch of transactions and a pointer to the previous block
-class Block:
+class Block(Hashable):
     def __init__(self, transactions, prev_block_hash, end_state_signature):
         self.prev_block_hash = prev_block_hash
         self.transactions = transactions
@@ -55,16 +59,13 @@ class Block:
         stringified_txs = '\t'.join([tx.__str__() for tx in self.transactions.values()])
         return '{0}\t{1}\t{2}'.format(self.prev_block_hash, self.end_state_signature, stringified_txs)
 
-    def hash(self):
-        return sha256(self.__str__()).hexdigest()
-
 # A transaction is the main way to "do something" in Ethereum. Transactions can be used to:
 # - Transfer ether
 # - Create accounts (including contract accounts)
 # - Call contract accounts (call the contract code and pass it some data)
 #
 # Each transaction changes the world state S via apply_transaction(S, TX) => S'
-class Transaction:
+class Transaction(Hashable):
     def __init__(self, sender_addr, receiver_addr, nonce, amount, data):
         self.sender_addr = sender_addr
         self.receiver_addr = receiver_addr
@@ -75,12 +76,9 @@ class Transaction:
     def __str__(self):
         return '{0},{1},{2},{3}'.format(self.sender_addr, self.receiver_addr, self.amount, self.data)
 
-    def hash(self):
-        return sha256(self.__str__()).hexdigest()
-
 # The above is mainly data models. Most of the interesting code begins here
-ROOT_ACCOUNT_ADDR = '0xdeadbeef'
-class BlockChain:
+ROOT_ACCOUNT_ADDR = 'deadbeef'
+class BlockChain(Hashable):
     PRE_GENESIS_BLOCK_HASH = '00000000'
     def genesis_block(self):
         return Block({}, self.PRE_GENESIS_BLOCK_HASH, self.genesis_world_state().signature())
@@ -178,7 +176,7 @@ class BlockChain:
         if tx.receiver_addr is None:
             if tx.data is None:
                 raise Exception('Contract creation must provide code via the data argument')
-            new_contract = Account(sha256(tx.data).hexdigest(), 0, 0, tx.data, {}, tx.hash())
+            new_contract = Account(Hashable.hash_fn(tx.data), 0, 0, tx.data, {}, tx.hash())
             state.accounts[new_contract.address] = new_contract
             return state
 
